7565c07c4dc01b865dd03ca2c7f51f57482b4a1c
Kira <kira.soba@gmx.de>
2024-06-22 23:13:24 +0200

    remove max-w-none from background design element

--- app/plant/[plantId]/layout.tsx
+++ app/plant/[plantId]/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     return <>
         <Header />
         <main className="max-w-screen-xl mb-5 md:m-auto">
-            <img src="/images/bg-element-mobile-1.png" alt="background design element" className="absolute -mt-5 -z-10 max-w-none" />
+            <img src="/images/bg-element-mobile-1.png" alt="background design element" className="absolute -mt-5 -z-10" />
             <div className=" mx-5 xl:mx-0 md:flex md:gap-20">
                 {children}
             </div>
