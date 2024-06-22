b71688a3095b74f272910462e4338c1906c34343
Kira <kira.soba@gmx.de>
2024-06-23 00:10:50 +0200

    make header logo redirect to dashboard

--- components/universals/header.tsx
+++ components/universals/header.tsx
@@ -6,10 +6,10 @@ export const Header: FC = () => {
     return (
         <header className="bg-white px-5 my-5">
             <div className="relative max-w-screen-xl m-auto flex justify-between items-center py-2">
-                <div className="flex">
+                <a href="/dashboard" className="flex">
                     <img src="/images/Illustration.png" alt="Illustration women with plant" className="hidden absolute h-16 w-16 bottom-0 md:block" />
                     <p className={clsx('text-4xl font-bold md:ps-16', dancingScript.className)}>FloraVenn</p>
-                </div>
+                </a>
                 <p className="text-highlight">Username</p>
             </div>
         </header>
